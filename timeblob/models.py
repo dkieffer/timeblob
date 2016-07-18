34b0c296e04d75106add4de46d158c8ce576b659
Eric Schultz <eschultz@prplfoundation.org>
2016-07-18 16:42:15 -0500

    Added two tests for verifying that when no timeentry is current, you'll get an error if you try to get current

--- timeblob/models.py
+++ timeblob/models.py
@@ -12,22 +12,24 @@ from . import exceptions
 
 class TimeEntry(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    description = models.TextField()
-    project = models.ForeignKey('Project')
-    task = models.ForeignKey('Task')
-    billable = models.BooleanField(default=False)
-    start = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False)
+    description = models.TextField(blank=True)
+    project = models.ForeignKey('Project',blank=True)
+    task = models.ForeignKey('Task',blank=True)
+    billable = models.BooleanField(default=False,blank=True)
+    start = models.DateTimeField(default=timezone.now, blank=False)
     stop = models.DateTimeField(blank=True, null=True)
     created_with = models.CharField(max_length=200)
-    last_updated = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(default=timezone.now, blank=False)
 
     def save(self, *args, **kwargs):
+        '''save updates last_updated'''
         self.last_updated = timezone.now
         super(TimeEntry, self).save(*args, **kwargs)
 
     @staticmethod
-    def current( user):
+    def current(user):
+        '''gets current time entry. If none current, raises `NoCurrentTask`'''
         try:
             return TimeEntry.objects.get(user=user, stop=None)
         except core_exceptions.ObjectDoesNotExist as e:
@@ -37,17 +39,18 @@ class TimeEntry(models.Model):
     @staticmethod
     def start_task( user, data):
         try:
-            current = current(user)
-            stop(user)
+            current = TimeEntry.current(user)
+            TimeEntry.stop(user)
         except exceptions.NoCurrentTask:
-            entry = TimeEntry(user=user, **data)
-            entry.save()
-            return entry
+            pass
+        entry = TimeEntry(user=user, **data)
+        entry.save()
+        return entry
 
 
     @staticmethod
     def stop_task(user):
-        current = current(user)
+        current = TimeEntry.current(user)
         current.stop = timezone.now
         current.save()
         return current
