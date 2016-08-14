ceb39360aef080cdf5b92840ded78a2f1a231c34
Eric Schultz <eschultz@prplfoundation.org>
2016-08-13 23:14:16 -0500

    dev.sh works if you're outside the directory

--- dev.sh
+++ dev.sh
@@ -1,4 +1,12 @@
-VENV=venv
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+VENV=$DIR/venv
 if [ ! -d $VENV ] ; then
     virtualenv $VENV
 else
@@ -11,9 +19,10 @@ fi
 
 
 (
-  cd timeblob/js
+  cd $DIR/timeblob/js
   npm install
-  gulp
+  node_modules/.bin/gulp
 )
 
-wait
+$VENV/bin/python $DIR/manage.py migrate
+$VENV/bin/python  $DIR/manage.py runserver
