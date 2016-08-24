29874a739ae74d2d09a80fb2d81eaa33b82115e2
Eric Schultz <eschultz@prplfoundation.org>
2016-08-23 21:41:16 -0500

    Change from MySQL to SQLite

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -75,23 +75,10 @@ sckuXINIU3DFWzZGr0QrqkuE/jyr7FXeUJj9B7cLo+s/TXo+RaVfi3kOc9BoxIvy
 
 
 apt-get update
-apt-get install -y -qq nginx mysql-server libmysqlclient-dev uwsgi uwsgi-plugin-python build-essential python-dev python-virtualenv git nodejs git-core g++
-apt-get build-dep -y -qq python-mysqldb
-# patch mysql conf to not change uid
-sed --in-place='' \
-        --expression='s/^user\t\t= mysql/#user\t\t= mysql/' \
-        /etc/mysql/my.cnf
-# patch mysql conf to use smaller transaction logs to save disk space
-cat <<EOF > /etc/mysql/conf.d/sandstorm.cnf
-[mysqld]
-# Set the transaction log file to the minimum allowed size to save disk space.
-innodb_log_file_size = 1048576
-# Set the main data file to grow by 1MB at a time, rather than 8MB at a time.
-innodb_autoextend_increment = 1
-EOF
+apt-get install -y -qq nginx uwsgi uwsgi-plugin-python build-essential python-dev python-virtualenv git nodejs git-core g++ sqlite3
+
+
 service nginx stop
-service mysql stop
 systemctl disable nginx
-systemctl disable mysql
 
 exit 0
--- timeblobsite/settings/production.py
+++ timeblobsite/settings/production.py
@@ -1,4 +1,6 @@
 from base import *
+import os
+
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
@@ -12,15 +14,75 @@ MIDDLEWARE_CLASSES = [
     'django_sandstorm.middleware.SandstormMiddleware'
 ]
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'app',
+#        'USER': 'app',
+#        'PASSWORD': 'app'
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'app',
-        'USER': 'app',
-        'PASSWORD': 'app'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join("/var/db", 'db.sqlite3'),
     }
 }
 
 STATIC_ROOT= '/var/app/static/'
 
 SANDSTORM = True
+DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/mysite.log',
+            'formatter': 'verbose'
+        },
+        'applogfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join('/var/log/', 'APPNAME.log'),
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10,
+        },
+        'requestlogfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join('/var/log/', 'request.log'),
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10,
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['requestlogfile'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django': {
+            'handlers':['applogfile'],
+            'propagate': True,
+            'level':'ERROR',
+        },
+        'timeblobsite': {
+            'handlers': ['applogfile'],
+            'level': 'ERROR',
+        },
+    }
+}
