32ea6803276c25c13fb94ea486adad9d58d5c47d
Eric Schultz <eschultz@prplfoundation.org>
2016-09-23 21:17:51 -0500

    WIP

---
+++ timeblob/js/services/timeentry.ts
@@ -0,0 +1,18 @@
+interface TimeEntry
+{
+  id: number
+  user: number
+  description: string
+  project: number
+  task: number
+  billable: boolean
+  start: Date
+  stop: Date
+  created_with: string
+  duration: number
+  last_update: Date
+}
+class TimeEntryService {
+
+
+}
