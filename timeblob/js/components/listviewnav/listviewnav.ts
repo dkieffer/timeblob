ccb46d89bbdd8a0a7d3977d66c7d4857e668a637
Eric Schultz <eschultz@prplfoundation.org>
2016-10-09 14:00:42 -0500

    Fix listviewnav so it shows up

--- timeblob/js/components/listviewnav/listviewnav.ts
+++ timeblob/js/components/listviewnav/listviewnav.ts
@@ -1,11 +1,25 @@
 import * as interfaces from '../../services/interfaces'
 import * as ng from 'angular'
+import CurrentEntrySensitiveComponent from '../current-sensitive';
+
 declare var TEMPLATE:any
+/**
+Only used in responsive
+*/
+export class ListViewNav
+{
+    listViewActive: boolean
+    toggleListView()
+    {
+      this.listViewActive = !this.listViewActive;
+    }
+}
+
 
 ng.module("app").component('listViewNav',
 {
   controller: ListViewNav,
   templateUrl: TEMPLATE.LISTVIEWNAV
 
-  
+
 });
