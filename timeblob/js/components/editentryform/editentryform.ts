ac0a754ce7aee5c7a5d7e515b8382703c89cc17d
David <david@kiefferbros.com>
2016-10-11 16:01:46 -0500

    Edit Entry attempt to add a done button

--- timeblob/js/components/editentryform/editentryform.ts
+++ timeblob/js/components/editentryform/editentryform.ts
@@ -3,10 +3,18 @@ import * as interfaces from '../../services/interfaces'
 import * as ng from 'angular'
 declare var TEMPLATE:any
 
-export class EditEntryForm {}
+export class EditEntryForm {
+    editEntry() {
+        this.onEditToggle()
+    }
+    onEditToggle : any
+}
 
 ng.module("app").component('editEntryForm',
 {
-  controller: EditEntryForm,
-  templateUrl: TEMPLATE.EDITENTRYFORM
+    controller: EditEntryForm,
+    templateUrl: TEMPLATE.EDITENTRYFORM,
+    bindings: {
+        onEditToggle: '&'
+    }
 });
