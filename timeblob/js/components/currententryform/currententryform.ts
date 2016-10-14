5a692a4c7c6e18e95734fc1535dd2d34a7aaf844
David <david@kiefferbros.com>
2016-10-14 17:04:50 -0500

    Started Layout Current Entry Form for handhelds

--- timeblob/js/components/currententryform/currententryform.ts
+++ timeblob/js/components/currententryform/currententryform.ts
@@ -4,6 +4,11 @@ import * as ng from 'angular'
 declare var TEMPLATE:any
 
 export class CurrentEntryForm {
+    currentEntryFormActive: boolean
+    toggleCurrentEntryForm()
+    {
+        this.currentEntryFormActive = !this.currentEntryFormActive
+    }
 }
 
 ng.module("app").component('currentEntryForm',
