b1dcf307e988dd1430e584fd969562fa4119d2dd
David <david@kiefferbros.com>
2016-12-16 11:05:25 -0600

    Manage View Front End
    layout for mobile and desktop with stand-in, mock content

--- timeblob/js/components/manageview/manageview.ts
+++ timeblob/js/components/manageview/manageview.ts
@@ -4,9 +4,45 @@ declare var TEMPLATE:any
 
 export class ManageView
 {
-    toggleManageView()
+    subListActive: boolean
+    projectListActive: boolean
+    clientListActive: boolean
+    orgListActive: boolean
+    x: number
+    toggleManageView(x)
     {
-        this.onManageViewToggle()
+        if (this.subListActive) {
+            this.toggleSubLists(x);
+            this.onManageViewToggle();
+        } else {
+            this.onManageViewToggle();
+        }
+    }
+
+    toggleSubLists(x)
+    {
+        if (x == 0) {
+            this.projectListActive = !this.projectListActive;
+            this.clientListActive = false;
+            this.orgListActive = false;
+        } else if (x == 1) {
+            this.clientListActive = !this.clientListActive;
+            this.projectListActive = false;
+            this.orgListActive = false;
+        } else if (x == 2) {
+            this.orgListActive = !this.orgListActive;
+            this.clientListActive = false;
+            this.projectListActive = false;
+        } else {
+            this.subListActive = !this.subListActive;
+            this.projectListActive = false;
+            this.clientListActive = false;
+            this.orgListActive = false;
+        }
+        if (this.orgListActive || this.clientListActive || this.projectListActive) {
+            this.subListActive = true;
+        }
+
     }
     onManageViewToggle: any
     manageViewActive: boolean
